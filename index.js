78826022436781042bd962ed1909033205689c54
Vitaliy Pylypchenko <vitaliy.py@thewhitelabelagency.com>
2024-08-12 17:12:46 +0300

    commit with data

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
+'use strict';
+
+const apiKey = 'b14107513b504604aa8c6226dc3d949d';
 const getWeatherBtn = document.querySelector('.getWeather');
-getWeatherBtn.addEventListener('click', getWeather);
+
+const weekDayNames = [
+    'Sunday',
+    'Monday',
+    'Tusday',
+    'Wednesday',
+    'Tursday',
+    'Friday',
+    'Saturday'
+]
+const monthsNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+getWeatherBtn.addEventListener('click', getWeatherByCoords);
 
 function getWeather(){
     const apiKey = 'b14107513b504604aa8c6226dc3d949d';
@@ -88,25 +116,65 @@ function getWeather(){
         // })
 }
 
-function displayWeather(data){
+async function getAsyncLocation(){
+    const city  = document.getElementById('city').value;
+    const limit  = 1;
 
-    const latCoord = data.lat;
-    const lonCoord = data.lon;
-    const cityName = data.name;
-    const cityState = data.state;
-
-
-    const locationData = {
-        lat: latCoord,
-        lon: lonCoord,
-        name:  cityName,
-        state: cityState
+    if(!city){
+        alert('Please enter a city');
+        return;
     }
 
-    return locationData;
+    const currentWeatherUrl  = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`
+
+    try{
+        const weatherResponse = await fetch(currentWeatherUrl);
+        const resp = await weatherResponse.json()
+        const {lat,lon} = resp[0];
+        const locationsCoords = {lat,lon};
+        return locationsCoords;
+    }catch(err){
+            console.error('Error is in async' , err);
+    }
+}
+
+
+async function getWeatherByCoords(){
+
+    const locationCoords = await getAsyncLocation();
+    if (!locationCoords) {
+        console.error('Failed to get location coordinates.');
+        return;
+    } else {
+
+        const {lat,lon} = locationCoords;
+
+        const currentWeatherByLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        const weatherResponse = await fetch(currentWeatherByLocation);
+        const asyncWeatherData = await weatherResponse.json();
+
+
+        const forecatsWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+        const forecastWeatherResponse = await fetch(forecatsWeatherUrl);
+        const asyncForecastWeatherData = await forecastWeatherResponse.json();
+
+
+        locationWeather(asyncWeatherData);
+        displayHourlyForecast(asyncForecastWeatherData);
+    }
 }
 
 function locationWeather(weatherData){
+   
+    if (!weatherData) {
+        console.error('No weather data available.');
+        return;
+    }
+
+    // console.log('Data in Current Location Weather');
+    // console.log(weatherData);
+
+
     const tempDivInfo  = document.getElementById('temp-div');
     const weatherInfoDiv  = document.getElementById('weather-info');
     const weatherIcon  = document.getElementById('weather-icon');
@@ -118,34 +186,43 @@ function locationWeather(weatherData){
     weatherInfoDiv.innerHTML = '';
     hourlyForecastDiv.innerHTML = '';
     tempDivInfo.innerHTML = '';
-if(weatherData.cod === '404'){
-    weatherInfoDiv.innerHTML = `<p> ${weatherData.message}</p>`
-}else{
-    const cityName = weatherData.name;
-    const temperature = Math.round(weatherData.main.temp - 273.15);
-    const description = weatherData.weather[0].description;
-    const iconCode = weatherData.weather[0].icon;
-    const iconUrl  = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+        if(weatherData.cod === '404'){
+             weatherInfoDiv.innerHTML = `<p> ${weatherData.message}</p>`
+        }else{
+            const date = new Date(weatherData.dt * 1000);
+            const weekDayName = weekDayNames[date.getUTCDay()];
+            const month = monthsNames[date.getMonth()]; // get month Name
+            const day = date.getDate();            // get date
+            const year = date.getFullYear();       // get year
+
+            // Date scting
+            const formattedDate = `${weekDayName} ${day},${month}, ${year}`;
+            const cityName = weatherData.name;
+            const temperature = Math.round(weatherData.main.temp - 273.15);
+            const description = weatherData.weather[0].description;
+            const iconCode = weatherData.weather[0].icon;
+            const iconUrl  = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
 
-    const temperatureHTML = `
-    <p> ${temperature}C</p>
-    `;
+             const temperatureHTML = `
+                <p> ${temperature}°C</p>
+                `;
 
     
-    const weatherHtml = `
-    <p>${cityName}</p>
-    <p>${description}</p>
-    `;
+            const weatherHtml = `
+                <p>${cityName}</p>
+                <p>${formattedDate}</p>
+                <p>${description}</p>
+                `;
 
 
-      tempDivInfo.innerHTML = temperatureHTML;
-      weatherInfoDiv.innerHTML = weatherHtml;
-      weatherIcon.src = iconUrl;
-      weatherIcon.alt = description;
+            tempDivInfo.innerHTML = temperatureHTML;
+            weatherInfoDiv.innerHTML = weatherHtml;
+            weatherIcon.src = iconUrl;
+            weatherIcon.alt = description;
 
-      showImage();
-}
+            showImage();
+        }
 }
 
 function showImage(){
@@ -153,7 +230,6 @@ function showImage(){
     .getElementById('weather-icon');
     weatherIcon.style.display = 'block';
 }
-
 
 function displayHourlyForecast(json){
     console.log('here we go');
@@ -164,6 +240,8 @@ function displayHourlyForecast(json){
 
     next5Days.forEach(item => {
         const dateTime = new Date (item.dt * 1000);
+        const month = monthsNames[dateTime.getMonth()]; // get month Name
+        const day = dateTime.getDate();            // get date
         const hour = dateTime.getHours();
         const temperature = Math.round(item.main.temp - 273.15);
         const iconCode = item.weather[0].icon;
@@ -173,6 +251,7 @@ function displayHourlyForecast(json){
 
         const hourlyItemHtml = `
         <div class="hourly-item">
+         <span> ${day} ${month}</span>
         <span> ${hour}:00</span>
         <img src="${iconUrl}" alt="Hourly Weather Icon">
         <span> ${temperature}°C</snap>
@@ -181,4 +260,3 @@ function displayHourlyForecast(json){
         hourlyForecastDiv.innerHTML += hourlyItemHtml;
     });
 }
-
