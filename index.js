6cd266dc7392f8d3ce558ac32bb604a8760600fa
Vitaliy <57655991+Vitaliy230377@users.noreply.github.com>
2024-08-12 10:18:17 +0300

    Forecast commit

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const getWeatherBtn = document.querySelector('.getWeather');
-
 getWeatherBtn.addEventListener('click', getWeather);
-
-
-
 
 function getWeather(){
     const apiKey = 'b14107513b504604aa8c6226dc3d949d';
     const city  = document.getElementById('city').value;
     const limit  = 1;
+    const numberOfDays = 7;
 
     if(!city){
         alert('Please enter a city');
@@ -48,6 +45,40 @@ function getWeather(){
             console.log('Error fetching current weather data: ', error);
             alert('Error fetching current weather data. Please try again');
         })
+   
+
+
+        fetch (currentWeatherUrl)
+        .then( response =>  response.json())
+            // console.log(response);
+        .then(json =>  {
+            // displayWeather(json[0]);
+            const locationData = displayWeather(json[0]);
+            // const lat = locationData.lat;
+            // const lon = locationData.lon;
+
+            const {lat, lon} = locationData
+
+            console.log(lat);
+            console.log(lon);
+
+            if (!lat || !lon) {
+                alert('Coordinates not found for the selected city');
+                return;
+            }
+
+            const forecatsWeatherData = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+          
+            return fetch(forecatsWeatherData);
+        }) // [0] because it can be more than 1 city in object
+
+        .then(response => response.json())
+        .then(json => displayHourlyForecast(json))
+        .catch(error =>{
+            console.log('Error fetching current weather data: ', error);
+            alert('Error fetching current weather data. Please try again');
+        })
+
 
         // .then(response => {
         //     return response.json(); // Обрізаємо console.log тут
@@ -74,8 +105,6 @@ function displayWeather(data){
 
     return locationData;
 }
-
-
 
 function locationWeather(weatherData){
     const tempDivInfo  = document.getElementById('temp-div');
@@ -124,3 +153,32 @@ function showImage(){
     .getElementById('weather-icon');
     weatherIcon.style.display = 'block';
 }
+
+
+function displayHourlyForecast(json){
+    console.log('here we go');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+    // // const next24Hours = hourlyData.slice(0, 8);
+    console.log(json.list);
+    const next5Days = json.list;
+
+    next5Days.forEach(item => {
+        const dateTime = new Date (item.dt * 1000);
+        const hour = dateTime.getHours();
+        const temperature = Math.round(item.main.temp - 273.15);
+        const iconCode = item.weather[0].icon;
+        const iconUrl  = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        console.log(temperature);
+
+        const hourlyItemHtml = `
+        <div class="hourly-item">
+        <span> ${hour}:00</span>
+        <img src="${iconUrl}" alt="Hourly Weather Icon">
+        <span> ${temperature}°C</snap>
+        </div>
+        `;
+        hourlyForecastDiv.innerHTML += hourlyItemHtml;
+    });
+}
+
