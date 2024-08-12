0bb038b8e01859af78e16e2ae5688e522cdef0a6
Vitaliy Pylypchenko <vitaliy.py@thewhitelabelagency.com>
2024-08-12 17:52:21 +0300

    fix commit

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function getWeather(){
     }
 
     // const currentWetherUrl  = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`
-    const currentWeatherUrl  = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`
+    const currentWeatherUrl  = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=${limit}&appid=${apiKey}`
 
 
     fetch (currentWeatherUrl)
